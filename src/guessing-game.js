eb20a58e6c7d4752d62e5e4c4e576d31e968a1f2
Valeriya2511 <finsternis.ru@mail.ru>
2023-02-23 23:11:25 +0600

    feat: the solution of task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.initialNumber = min;
+        this.finiteNumber = max;
     }
-
+  
     guess() {
-
+      return this.average = Math.round((this.finiteNumber - this.initialNumber) / 2) + this.initialNumber;
     }
-
+  
     lower() {
-
+      return this.finiteNumber = this.average;
     }
-
+  
     greater() {
-
+      return this.initialNumber = this.average;
     }
 }
 
